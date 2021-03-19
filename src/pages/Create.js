c275c36be484ab56158e77a94779fde603db2fa7
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2021-03-19 11:38:04 +0400

    Own custom theme

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -5,19 +5,7 @@ import { Container } from '@material-ui/core';
 import SendIcon from '@material-ui/icons/Send';
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles({
-  btn: {
-    backgroundColor: 'violet',
-    fontSize: 25,
-    '&:hover': {
-      backgroundColor: 'crimson',
-    },
-  },
-  title: {
-    textDecoration: 'underline',
-    marginBottom: 20,
-  },
-});
+const useStyles = makeStyles({});
 
 export default function Create() {
   const classes = useStyles();
@@ -25,7 +13,6 @@ export default function Create() {
   return (
     <Container>
       <Typography
-        className={classes.title}
         variant='h6'
         component='h2'
         color='textSecondary'
@@ -34,7 +21,6 @@ export default function Create() {
       </Typography>
 
       <Button
-        className={classes.btn}
         onClick={() => console.log('Click')}
         variant='contained'
         type='submit'
