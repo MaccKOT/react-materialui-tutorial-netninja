8406eeadb66eee02821022681cf30a33dff6f61c
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2021-03-19 11:11:26 +0400

    Buttons and icons

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Typography } from '@material-ui/core';
+import { Button } from '@material-ui/core';
+import { Container } from '@material-ui/core';
+import SendIcon from '@material-ui/icons/Send';
 
 export default function Create() {
   return (
-    <div>
+    <Container>
       <Typography
         variant='h6'
         component='h2'
@@ -11,6 +14,16 @@ export default function Create() {
         gutterBottom>
         Create a New Note
       </Typography>
-    </div>
+
+      <Button
+        onClick={() => console.log('Click')}
+        variant='contained'
+        type='submit'
+        color='secondary'
+        disableElevation
+        endIcon={<SendIcon />}>
+        Submit
+      </Button>
+    </Container>
   );
 }
